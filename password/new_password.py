86d30e4409898ebbdd5beff0f23ce7b4d9ca8acd
KodGlobal <120708212+KodGlobal@users.noreply.github.com>
2025-04-01 03:17:14 +0300

    Update new_password.py

--- password/new_password.py
+++ password/new_password.py
@@ -2,13 +2,13 @@ import random
 import string
 
 def generate_password(length=12):
-    """Generating a random password of a specified length."""
+    """Generating a random password of specified length."""
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''
     for i in range(length):
         password += random.choice(characters)
     return password
 
-# Am example of use
+# An example of use
 password_length = 12  # You can choose any password length
 print("Your new password:", generate_password(password_length))
